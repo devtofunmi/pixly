b611f3dbd6a03975791d6e6bd778a9c3d5c7a888
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-31 19:55:46 +0100

    ..

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  env: {
+    UNSPLASH_KEY: process.env.UNSPLASH_KEY,
+    GIPHY_KEY: process.env.GIPHY_KEY
+  },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'media.giphy.com',
+      'giphy.com',
+      'i.giphy.com',
+      'giphyusercontent.com',
+      "media0.giphy.com",
+      "media1.giphy.com",
+      "media2.giphy.com",
+      "media3.giphy.com",
+      "media4.giphy.com",
+      "media5.giphy.com",
+    ],
+  },
+  experimental: {
+  },
 };
 
 export default nextConfig;
