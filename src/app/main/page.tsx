f1b1de349c221b955daad239bb5348d9aae9bfcd
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-06-07 14:17:00 +0100

    Update page.tsx

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -76,7 +76,7 @@ const Home = () => {
       {/* Search Sectionn */}
       <div className="max-w-4xl mx-auto mb-12">
         <h1 className="text-4xl font-bold mb-4 text-center">Find Matching Images & GIFs</h1>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-5">
           <div className="relative w-full">
             <input
               type="text"
